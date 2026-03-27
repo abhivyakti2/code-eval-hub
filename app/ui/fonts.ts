fdb21fa13c2ee498a35c96c08d605ccd46cd6585
Abhivyakti Pal <143368698+abhivyakti2@users.noreply.github.com>
2026-03-27 10:03:50 +0530

    Initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+// fonts used in the application
+import {Inter, Lusitana} from 'next/font/google';
+export const inter = Inter({subsets : ['latin']});
+export const lusitana = Lusitana({subsets : ['latin'], weight : ['400', '700']});
